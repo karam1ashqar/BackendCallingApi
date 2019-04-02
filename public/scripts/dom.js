9995edf1c54ac14f4ae26ea4c2099cbaba2b4489
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-02 15:53:44 +0300

    fetch url is up, waiting for returning value from backend

--- public/scripts/dom.js
+++ public/scripts/dom.js
@@ -4,6 +4,30 @@ var inputFieldValue = document.getElementById("searchInput");
     submitButton.addEventListener("click", function(event) {
       event.preventDefault();
 
-       inputFieldValue.value = "";
+      myFetch( inputFieldValue.value );
+
+      inputFieldValue.value = "";
+
 
 });
+
+function myFetch( value ) {
+
+    fetch('/search?s=' + inputFieldValue.value)
+     .then(
+      (response) => {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+
+        response.json().then((data) => {
+          console.log(data)
+        })
+      }
+    )
+    .catch((err) => {
+      console.log('Fetch Error :-S', err);
+    });
+}
