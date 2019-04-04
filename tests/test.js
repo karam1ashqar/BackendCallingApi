c616bcca31c9a699be5c088ce2468781915bc5a5
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-04 12:53:31 +0300

    Added nock to mock test, testing if title got from the API is equaled to our desired title

--- tests/test.js
+++ tests/test.js
@@ -1,13 +1,17 @@
 const test = require('tape');
 const supertest = require('supertest');
 const router = require('../src/router');
+const nock = require('nock');
 
 test('Server is up', (t) => {
   let num = 2;
   t.equal(num, 2, 'Should return 2 - meaning server started');
   t.end();
+})
 
-test('Home route returns a status code of 200', { timeout:100 }, (t) => {
+
+
+test('Home route returns a status code of 200', (t) => {
     supertest(router)
         .get("/")
         .expect(200)
@@ -16,22 +20,24 @@ test('Home route returns a status code of 200', { timeout:100 }, (t) => {
             t.error(err);
 
       t.equal(res.statusCode, 200, 'Should return 200');
+      t.end()
 
     });
 });
 
-test('404 not found route returns a status code of 404', { timeout:100 }, (t) => {
+test('404 not found route returns a status code of 404', (t) => {
     supertest(router)
-        .get("/sd")
+        .get("/sdfsfs")
         .expect(404)
         .end((err, res) => {
             t.error(err);
             t.equal(res.statusCode, 404, 'Should return 404');
+            t.end()
 
         });
 });
 
-test('css file route returns a status code of 200', { timeout:100 }, (t) => {
+test('css file route returns a status code of 200', (t) => {
     supertest(router)
         .get("/public/styles/styles.css")
         .expect(200)
@@ -45,20 +51,28 @@ test('css file route returns a status code of 200', { timeout:100 }, (t) => {
         });
 });
 
-test('API request', { timeout:400 }, (t) => {
+test('API request', (t) => {
+
+    const scope = nock('https://backendcallingapi.herokuapp.com')
+      .get('/search?s=batman&apikey=4a8cc460')
+      .delay(2000)
+      .reply(200, {Response:"True"})
+
+
     supertest(router)
         .get("/search?s=batman")
         .expect(200)
         .expect('Content-Type', /json/)
         .end((err, res) => {
             t.error(err);
-            t.equal(res.statusCode, 200, 'Should return 200');
+            t.equal(res.body[0].Title, "Batman Begins", `Title should equal to ${res.body[0].Title}`);
+            nock.cleanAll()
             t.end();
 
         });
 });
 
-test('500 error', { timeout:400 }, (t) => {
+test('500 error', (t) => {
     supertest(router)
         .get("/selectedss")
         .expect(500)
@@ -70,20 +84,22 @@ test('500 error', { timeout:400 }, (t) => {
         });
 });
 
-test('handle item', { timeout:500 }, (t) => {
+test('handle item', (t) => {
+
     supertest(router)
         .get("/search?i=batman")
         .expect(200)
         .expect('Content-Type', /json/)
         .end((err, res) => {
             t.error(err);
+
             t.equal(res.statusCode, 200, 'Should return 200');
             t.end();
 
         });
 });
 
-test('responsiveness', { timeout:500 }, (t) => {
+test('responsiveness', (t) => {
     supertest(router)
         .get("/public/styles/responsiveness.css")
         .expect(200)
@@ -96,7 +112,7 @@ test('responsiveness', { timeout:500 }, (t) => {
         });
 });
 
-test('index.html gives 500 error', { timeout:500 }, (t) => {
+test('index.html gives 500 error', (t) => {
     supertest(router)
         .get("/public/layouts/index.html")
         .expect(500)
@@ -108,7 +124,7 @@ test('index.html gives 500 error', { timeout:500 }, (t) => {
         });
 });
 
-test('index.html gives 500 error', { timeout:500 }, (t) => {
+test('index.html gives 500 error', (t) => {
     supertest(router)
         .get("/selected")
         .expect(200)
