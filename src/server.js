b4ba5ce6fb20bd65c85e25bb6eec75a659df2774
obaydah <obayda7h@gmail.com>
2019-04-04 10:39:55 +0300

    fixes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const router = require("./router");
-const port = process.env.PORT || 4000;
+const env = require('env2')('src/.env');
+
+const port = process.env.PORT;
 const server = http.createServer(router);
 
 server.listen(port, () => {
